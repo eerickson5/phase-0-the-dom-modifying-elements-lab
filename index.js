580d1352b1fa839baea800f6599461334fa40f9f
Elise Erickson <elisece717@gmail.com>
2023-10-30 20:53:15 -0400

    this was fun

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let mainElement = document.getElementById('main');
+mainElement.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'ELISE is the champion';
+document.body.append(newHeader);
